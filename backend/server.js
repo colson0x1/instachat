245d52c9e66c5897a278f7b82f8ea21c89e58df1
Colson <colsoncalifornication@gmail.com>
2024-01-31 22:51:46 +0545

    implement auth controllers + routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 dotenv.config();
 
@@ -9,5 +11,7 @@ const PORT = process.env.PORT || 4000;
 app.get('/', (req, res) => {
   res.send('<h1 style="color: #1DB954">instaChat</h1>');
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
