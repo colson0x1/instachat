6a37fdce0de16367875188bb7b73b355095681a5
Colson <colsoncalifornication@gmail.com>
2024-01-31 23:16:07 +0545

    configure MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/mongoDB.config.js';
 
 const app = express();
 dotenv.config();
@@ -14,4 +15,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
+});
