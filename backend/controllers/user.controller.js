69feb26eef44e875ea3e6a533142bc05fcc05f72
Colson <colsoncalifornication@gmail.com>
2024-02-03 14:56:29 +0545

    implement get users for sidebar endpoint

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,18 @@
+import User from '../models/user.model.js';
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user_id;
+
+    // Get every users from the DB except the currenly logged in user
+    // We don't want to load our profile in the users list on message dashboard
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select('-password');
+
+    res.status(200).json(filteredUsers);
+  } catch (err) {
+    console.log('Error in getUsersForSidebar: ', err.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
