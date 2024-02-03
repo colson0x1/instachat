8174cb1274e8860ff186e5c715010fe718374db5
Colson <colsoncalifornication@gmail.com>
2024-02-03 20:39:19 +0545

    add signup component layout

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,5 +1,74 @@
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+          SignUp
+          <span className='text-blue-500'> instaChat</span>
+        </h1>
+
+        <form>
+          <div>
+            <label htmlFor='' className='label p-2'>
+              <span className='text-base label-text'>Name</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Colson'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='' className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input
+              type='text'
+              placeholder='colson'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+
+          <div>
+            <label htmlFor='' className='label'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Enter password'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+          <div>
+            <label htmlFor='' className='label'>
+              <span className='text-base label-text'>Confirm Password</span>
+            </label>
+            <input
+              type='password'
+              placeholder='Confirm password'
+              className='w-full input input-bordered h-10'
+            />
+          </div>
+
+          {/* GENDER CHECKBOX */}
+
+          <a
+            href='#'
+            className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
+          >
+            Already have an account?
+          </a>
+
+          <div>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
