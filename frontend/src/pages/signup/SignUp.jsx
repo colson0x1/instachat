6ab2690e8fa08f09bca9650eef6dd93ab9a849e3
Colson <colsoncalifornication@gmail.com>
2024-02-03 20:48:19 +0545

    add gender component

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,3 +1,5 @@
+import GenderCheckbox from './GenderCheckbox';
+
 const SignUp = () => {
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
@@ -51,7 +53,7 @@ const SignUp = () => {
             />
           </div>
 
-          {/* GENDER CHECKBOX */}
+          <GenderCheckbox />
 
           <a
             href='#'
