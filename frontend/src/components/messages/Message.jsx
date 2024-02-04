0527f2440bf04a87dd4fea79de7d5d81e0c15231
Colson <colsoncalifornication@gmail.com>
2024-02-05 00:22:54 +0545

    render sender, receiver photo and different color on dashboard + format sent message time

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,20 +1,31 @@
-const Message = () => {
+import { useAuthContext } from '../../context/AuthContext';
+import { extractTime } from '../../utils/extractTime';
+import useConversation from '../../zustand/useConversation';
+
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500' : '';
+
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
       <div className='chat-image avatar'>
         <div className='w-10 rounded-full'>
-          <img
-            src='https://drupal8-prod.visitcalifornia.com/sites/drupal8-prod.visitcalifornia.com/files/2020-07/SFBA_San%20Francisco_Golden%20Gate%20Bridge.jpeg'
-            alt='user avatar'
-          />
+          <img src={profilePic} alt='user avatar' />
         </div>
       </div>
 
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Yo! What is upp?
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
       </div>
       <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>
-        11:11
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -8,6 +8,12 @@ const Messages = () => {
 
   return (
     <div className='px-4 flex-1 overflow-auto'>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
       {!loading && messages.length === 0 && (
