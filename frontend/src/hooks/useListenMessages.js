6715b1836c09696e18d8cd22a0b4cec12e0d4349
Colson <colsoncalifornication@gmail.com>
2024-02-05 11:02:59 +0545

    add message notification sound

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,6 +2,8 @@ import { useEffect } from 'react';
 import { useSocketContext } from '../context/SocketContext';
 import useConversation from '../zustand/useConversation';
 
+import notificationSound from '../assets/sounds/notification.mp3';
+
 const useListenMessages = () => {
   // Return online users as well as the sockets
   const { socket } = useSocketContext();
@@ -11,9 +13,12 @@ const useListenMessages = () => {
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
       newMessage.shouldShake = true;
+      const sound = new Audio(notificationSound);
+      sound.play();
       setMessages([...messages, newMessage]);
     });
 
+    // important so that we're not listening for this event more than once
     return () => socket?.off('newMessage');
   }, [socket, setMessages, messages]);
 };
