891d1f0afb368357c2b02c64035850d16f64c758
Colson <colsoncalifornication@gmail.com>
2024-02-05 10:55:29 +0545

    add shake animation to the incoming message

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -10,6 +10,7 @@ const useListenMessages = () => {
 
   useEffect(() => {
     socket?.on('newMessage', (newMessage) => {
+      newMessage.shouldShake = true;
       setMessages([...messages, newMessage]);
     });
 
